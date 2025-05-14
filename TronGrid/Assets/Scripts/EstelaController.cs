f86d86891036265ec1fb8a1b0bf8f3f1f6b94b9a
Fenrro27 <126926727+Fenrro27@users.noreply.github.com>
2025-05-14 13:01:50 +0200

    Estela incompleta

---
+++ TronGrid/Assets/Scripts/EstelaController.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class EstelaController : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
